183df09cf87fac6e52475b69c435e9a6d7efa65b
nthitz <nthitz@gmail.com>
2015-10-06 21:15:02 +0000

    perhaps full importing

--- scripts/importPlugPlaylists.js
+++ scripts/importPlugPlaylists.js
@@ -19,6 +19,7 @@ pg.connect(process.env.PG_CONNECTION, function(err, client, done) {
     insertResults(results)
   })
 
+  var songLookup = {}
   function insertResults(results) {
 
     var entriesByUser = _(results).map(function(entry) {
@@ -32,16 +33,22 @@ pg.connect(process.env.PG_CONNECTION, function(err, client, done) {
       return data.username
     }).value()
 
-    var users = Object.keys(entriesByUser);
-    var lastUserEntry = _(users)
+    var users = _(Object.keys(entriesByUser))
+      .map(function(user) {
+        return {username: user}
+      }).value();
+    var lastUserEntries = _(users)
         .map(function(user) {
-          var lastUserEntryIndex = entriesByUser[user].length - 1
-          return entriesByUser[user][lastUserEntryIndex]
+          var lastUserEntryIndex = entriesByUser[user.username].length - 1
+          var lastEntry = entriesByUser[user.username][lastUserEntryIndex]
+          lastEntry.user = user;
+          return lastEntry
         }).value()
+
 
     // we now have the last entry that each user submitted. that's a good thing
 
-    var songs = _(lastUserEntry)
+    var songs = _(lastUserEntries)
       .map(function(entry) {
         // get just the playlists
         return entry.playlists
@@ -52,27 +59,152 @@ pg.connect(process.env.PG_CONNECTION, function(err, client, done) {
         return playlist.songs.data
       })
       .flatten() //merge arrays
-      .uniq() //remove dupes
+      .uniq('cid') //remove dupes
       .value()
 
-    insertSongs(songs)
+    deleteStuff()
       .then(function() {
-         done()
-         console.log('done');
-         process.exit(0)
+        return insertSongs(songs)
+      })
+      .then(function() {
+        return insertUsers(users)
+      })
+      .then(function() {
+        return insertPlaylists(lastUserEntries)
+      }).then(function() {
+        done()
+        console.log('done');
+        process.exit(0)
+      }, function() {
+        console.log('error');
+        console.log(arguments)
       })
   }
-  function insertSongs(songs) {
+
+  function deleteStuff() {
     return new Promise(function(resolve, reject) {
-      var numInserted = 0
-      deleteSongs()
-      function deleteSongs() {
-        client.query('DELETE FROM song', function() {
-          client.query('ALTER SEQUENCE song_id_seq RESTART WITH 1', insertSong)
+      deletePlaylists()
+      function deletePlaylists() {
+        console.log('resetting playlists')
+        client.query('DELETE FROM "playlist_has_song"', function() {
+          client.query('DELETE FROM "playlist"', function() {
+            client.query('ALTER SEQUENCE "playlist_id_seq" RESTART WITH 1', deleteUsers)
+          })
         })
       }
+      function deleteUsers() {
+        console.log('resetting users')
+        client.query('DELETE FROM "user"', function() {
+          client.query('ALTER SEQUENCE user_id_seq RESTART WITH 1', deleteSongs)
+        })
+      }
+      function deleteSongs() {
+        console.log('resetting songs')
+        client.query('DELETE FROM "song"', function() {
+          client.query('ALTER SEQUENCE song_id_seq RESTART WITH 1', function() {
+            resolve()
+          })
+        })
+      }
+    });
+  }
+  function insertPlaylists(entries) {
+    return new Promise(function(resolve, reject) {
+      var numEntriesInserted = 0;
+      insertEntry()
+      function insertEntry() {
+        var nextEntry = entries[numEntriesInserted];
+        console.log('nextEntry')
+        Promise.all(_.map(nextEntry.playlists, function(playlist) {
+          return new Promise(function(playlistResolve, playlistReject) {
+            //insert playlist get ID
+            client.query({
+              text: 'INSERT INTO "playlist" (name, user_id) VALUES ($1, $2) RETURNING id',
+              values: [playlist.name, nextEntry.user.id],
+              callback: function (error, result) {
+                if (error) {
+                  console.log('error');
+                  console.log(error)
+                  reject()
+                  return
+                }
+                playlist.id = result.rows[0].id
+                console.log(playlist.name)
+                Promise.all(_.map(playlist.songs.data, function(song) {
+                  // console.log(song)
+                  return new Promise(function(songResolve, songReject) {
+                    client.query({
+                      text: 'INSERT INTO "playlist_has_song" (playlist_id, song_id) VALUES ($1, $2)',
+                      values: [playlist.id, songLookup[song.cid]],
+                      callback: function(error, result) {
+                        if (error) {
+                          console.log('error');
+                          console.log(error);
+                          songReject();
+                          return
+                        }
+                        songResolve()
+                      }
+                    })
+                  })
+                }))
+                .then(function() {
+                  playlistResolve()
+                })
+              }
+            })
+          })
+        })).then(function() {
+          numEntriesInserted++;
+          if (numEntriesInserted === entries.length) {
+            resolve()
+          } else {
+            insertEntry()
+          }
+        })
+      }
+    })
+    // hmm I'm sorry about this ^^^^
+  }
+
+  function insertUsers(users) {
+    console.log('insert users')
+    return new Promise(function(resolve, reject) {
+      var numInserted = 0;
+      insertUser()
+      function insertUser() {
+        console.log('inserting user ' + numInserted);
+        var nextUser = users[numInserted]
+        client.query({
+          text: 'INSERT INTO "user" (username) VALUES ($1) RETURNING id',
+          values: [nextUser.username],
+          callback: function(error, result) {
+            if (error) {
+              console.log('error');
+              console.log(error);
+              reject(error);
+              return
+            }
+            nextUser['id'] = result.rows[0].id
+            numInserted++;
+            if (numInserted === users.length) {
+              console.log('users inserted');
+              resolve()
+            } else {
+              insertUser()
+            }
+          }
+        })
+      }
+    })
+  }
+  function insertSongs(songs) {
+    console.log('insert songs')
+    return new Promise(function(resolve, reject) {
+      var numInserted = 0
+      insertSong()
       function insertSong() {
-        console.log('inserting' + numInserted)
+        console.log('inserting song ' + numInserted)
         var nextSong = songs[numInserted];
         var params = [
           nextSong['id'],
@@ -93,7 +225,7 @@ pg.connect(process.env.PG_CONNECTION, function(err, client, done) {
               reject(err)
               return;
             }
-            nextSong['id'] = result.rows[0]['id']
+            songLookup[nextSong['cid']] = result.rows[0]['id']
             numInserted++
             if (numInserted === songs.length) {
               insertFinished()
@@ -104,9 +236,8 @@ pg.connect(process.env.PG_CONNECTION, function(err, client, done) {
         })
       }
       function insertFinished() {
-        console.log('insert finished')
-        // should do something with a promise here.
-        resolve()
+        console.log('songs finished')
+        resolve(true)
       }
     })
   }
