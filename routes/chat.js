cda6e2efe5efc4f7ea444bae63b5846743ef3b8a
nthitz <nthitz@gmail.com>
2015-10-15 04:31:46 +0000

    don't let people impersonate users in chat

--- routes/chat.js
+++ routes/chat.js
@@ -1,17 +1,18 @@
-var socket = null;
-var app = null;
-
 var expressWs = require('express-ws')
+
+var app = null;
 
 function initChatSocket(_app) {
   app = _app;
   expressWs = expressWs(app)
   app.ws('/chatWs', function(ws, request) {
-    socket = ws
-    // s// console.log(ws)
     ws.on('message', function(message) {
       var messageObject = JSON.parse(message)
       if (messageObject.type === 'chat') {
+        if(messageObject.data.userId !== request.user.id) {
+          console.log('someone trying to impersonate chat messages?')
+          return
+        }
         expressWs.getWss('/chatWs')
           .clients.forEach(function(client) {
             client.send(message)
