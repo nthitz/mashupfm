5caa177ed74eb3892ace524c636b8ef211e38e1e
nthitz <nthitz@gmail.com>
2015-11-10 04:40:33 +0000

    start of a user queue

--- app/routes/playback.js
+++ app/routes/playback.js
@@ -1,6 +1,7 @@
 'use strict'
 var express = require('express');
 var router = express.Router();
+var _ = require('lodash')
 
 var db = require('../db')
 var ServerActions = require('../ServerActions')
@@ -10,7 +11,11 @@ var songStartedAt = -1
 var userIDs = [1];
 var nextSongTimeout = null
 
-function getNextSong() {
+let queue = [];
+let currentPlayingUser = null
+
+
+function getDefaultSong() {
   return db.query('SELECT * FROM "playlist" WHERE user_id IN (' + userIDs.join(',') + ')')
     .then(function(result) {
       return result.rows.reduce(function(prev, next) {
@@ -38,15 +43,51 @@ function getNextSong() {
         process.nextTick(getNextSong)
         return
       }
-      currentSong = song;
-      console.log(currentSong)
-      songStartedAt = Date.now()
-      clearTimeout(nextSongTimeout)
-      nextSongTimeout = setTimeout(getNextSong, song.duration * 1000)
-      return currentSong
+      return song
     })
 }
 
+function getNextSong() {
+  let onNewSong = (song) => {
+    console.log(song)
+    songStartedAt = Date.now()
+    clearTimeout(nextSongTimeout)
+    nextSongTimeout = setTimeout(getNextSong, song.duration * 1000)
+    currentSong = song
+    return currentSong
+  }
+  if (queue.length === 0 && currentPlayingUser === null) {
+    return getDefaultSong()
+      .then(onNewSong)
+  }
+  if (currentPlayingUser !== null) {
+    queue.push(currentPlayingUser)
+  }
+  currentPlayingUser = queue.shift()
+  return db.query(
+    `SELECT playlist.sort, "user".active_playlist_id, song.*
+      FROM "user", playlist, song
+      WHERE "user".id=$1 AND
+        playlist.id="user".active_playlist_id AND
+        song.id=playlist.sort[1]
+    `,
+    [currentPlayingUser.id]
+  ).then((result) => {
+    let nextSong = result.rows[0]
+    let sort = nextSong.sort
+    let playlistId = nextSong.active_playlist_id
+
+    delete nextSong.sort
+    delete nextSong.active_playlist_id
+
+    // need to update order property on playlist
+    // need to sync data to user
+    return nextSong
+  }).then(onNewSong)
+
+
+
+}
 function skip() {
   clearTimeout(nextSongTimeout)
   getNextSong()
@@ -63,6 +104,52 @@ router.get('/currentSong', function(request, result) {
     song: currentSong,
     seek: seek,
   })
+})
+
+router.get('/joinQueue', function(request, response) {
+  if (!request.user) {
+    response.status(500).send('nope')
+  }
+  let inQueue = _.find(queue, (user) => {
+    return user.id === request.user.id
+  })
+
+  if (inQueue) {
+    return response.json({error: 'already in queue'})
+  }
+
+  queue.push({
+    username: request.user.username,
+    id: request.user.id,
+  })
+  response.json({
+    status: 'success',
+    currentQueue: queue,
+  })
+})
+
+router.get('/leaveQueue', function(request, response) {
+  if (!request.user) {
+    response.status(500).send('nope')
+  }
+  let queueIndex = _.findIndex(queue, (user) => {
+    return user.id === request.user.id
+  })
+
+  if (queueIndex === -1) {
+    return response.json({error: 'not in queue'})
+  }
+
+  queue.splice(queueIndex, 1)
+
+  if (queue.length === 0) {
+    currentPlayingUser = null
+  }
+  response.json({status: 'success'})
+})
+
+router.get('/currentQueue', function(request, response) {
+  response.json(queue)
 })
 
 ServerActions.forceSkip.listen(skip)
--- app/scripts/MainViews/QueueControls.js
+++ app/scripts/MainViews/QueueControls.js
@@ -1,28 +1,53 @@
 import React from 'react'
+import request from 'superagent'
 
 export default class QueueControls extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-
+      userPosition: null,
+      totalUsers: null,
+      inQueue: false
     }
   }
 
+  _toggleJoin(event) {
+    let inQueue = !this.state.inQueue
+    this.setState({
+      inQueue: inQueue
+    })
+    let endpoint = inQueue ? '/joinQueue' : '/leaveQueue'
+    request.get(endpoint)
+      .end((error, response) => {
+        console.log(arguments)
+      })
+  }
+
   render() {
+    let joinButtonClass = this.state.inQueue ?
+      'leave-queue-icon' : 'join-queue-icon'
+
+    let queueInfo = null
+
+    if (this.state.userPosition !== null) {
+      queueInfo = (
+        <div id='queue-info'>
+          <div id='queue-position'>
+            {this.state.userPosition}
+          </div>
+          <div id='queue-total'>
+            {this.state.totalUsers}
+          </div>
+        </div>
+      )
+    }
     return (
-      <div id="queue-controls">
-        <div id="join-queue" className="join-queue-icon">
-        </div>
-        <div id="queue-info">
-          <div id="queue-position">
-            2
-          </div>
-          <div id="queue-total">
-            10
-          </div>
-        </div>
-        <div id="leave-queue"></div>
+      <div id='queue-controls' className={this.state.inQueue ? 'active' : ''}>
+        <div id='join-queue'
+          onClick={this._toggleJoin.bind(this)}
+          className={joinButtonClass}></div>
+        {queueInfo}
       </div>
 
     )
